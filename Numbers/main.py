7fa28711e3599420315c289fe78f4f61ed7e5f9a
amesp73 <amitrpatel502@gmail.com>
2024-04-15 19:11:26 -0500

    Computer guesses number using randint and if else loops

--- Numbers/main.py
+++ Numbers/main.py
@@ -16,4 +16,23 @@ def guess(x):
     
     print('Congrats! You guessed the correct number!')
 
-guess(50)
+# guess(50)
+    
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low #could also be high b/c low = high
+        feedback = input(f'Is {guess} too high (H), too low(L), or correct(C)?? ').lower()
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+
+    print('Yay! The computer guessed your number correctly!')
+
+computer_guess(50)
